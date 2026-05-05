b9b207535e8d3d717ce461b3e460d252e942ebd0
Mozzie <mozzie009@gmail.com>
2026-05-05 12:37:06 -0600

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,309 @@
+//.
+const version = "1.9-indev";
+let selected_scene = 0;
+let iframe;
+let player;
+let ui_scale = 2;
+let current_timestamp
+
+let toolbar = {
+  width: 50,
+  height: 200,
+  visible: false,
+  posX: 0,
+  posY: 400,
+};
+
+let loop_data = {
+  start: "0:24:0",
+  end: "0:24:1",
+  enabled: false,
+};
+
+var render_scale = 1;
+let video_width;
+let video_height;
+const video_ratio = 1.777;
+
+function setup() {
+  iframe = document.getElementById("vimeo-player");
+  player = new Vimeo.Player(iframe);
+
+  player.on("timeupdate", (data) => {
+    current_timestamp = data.seconds;
+
+    if (loop_data.enabled) {
+      if (current_timestamp >= to_seconds(loop_data.end)) {
+        player.setCurrentTime(to_seconds(loop_data.start));
+        console.log("jumping to previous loop start");
+      }
+    }
+
+    current_timestamp = time_as_string(current_timestamp);
+  });
+  load_scenes();
+  create_ui();
+  show_toolbar();
+  hide_toolbar();
+  selected_scene_changed();
+
+  createCanvas(float(iframe.width), float(iframe.height));
+  background(150);
+}
+
+function draw() {
+  text(version, 0, 0);
+  // background(random()*255, random()*255, random()*255)
+}
+
+function create_ui() {
+  ui_drop_down = createButton("Tools");
+  ui_drop_down.position(0, 0);
+  ui_drop_down.size(60 * ui_scale, 20 * ui_scale);
+  ui_drop_down.mousePressed(pressed_drop_down);
+  ui_drop_down.style("font-size", 10 * ui_scale + "px");
+
+  ui_select_scene = createSelect("Select Scene");
+  ui_select_scene.position(10000, 10000);
+  ui_select_scene.changed(selected_scene_changed);
+
+  ui_select_part = createSelect("Select Part");
+  ui_select_part.position(10000, 10000);
+  ui_select_part.changed(selected_part_changed);
+
+  ui_jump_scene = createButton("Jump");
+  ui_jump_scene.position(10000, 10000);
+  ui_jump_scene.size();
+  ui_jump_scene.mousePressed(pressed_jump_scene);
+
+  ui_input_time = createInput();
+  ui_input_time.value("0:45:2");
+
+  ui_jump_time = createButton("Jump");
+  ui_jump_time.position(10000, 10000);
+  ui_jump_time.size();
+  ui_jump_time.mousePressed(pressed_jump_time);
+
+  for (let i = 0; i < scenes.length; i++) {
+    ui_select_scene.option(scenes[i].name, i);
+  }
+  
+   // ui_select_scene.option("Custom",scenes. length)
+  
+  ui_save_timestamp = createButton("+ Timestamp");
+  ui_save_timestamp.position(10000, 10000);
+  ui_save_timestamp.size();
+  ui_save_timestamp.mousePressed(pressed_save_timestamp);
+  
+  ui_save_timestamp_name = createInput("Enter Timestamp Name");
+  ui_save_timestamp_name.position(10000, 10000);
+  ui_save_timestamp_name.size();
+  
+  ui_text_custom_features = createP("Custom Features")
+  ui_text_custom_features. position(10000,10000)
+  ui_text_custom_features. size()
+  ui_text_custom_features.style("background", "rgba(180, 220, 220, 1)")
+}
+
+function selected_scene_changed() {
+  empty_select();
+  selected_scene = ui_select_scene.value();
+  console.log("changed scene");
+  for (let i = 0; i < scenes[selected_scene].parts.length; i++) {
+    ui_select_part.option(scenes[selected_scene].parts[i].name, i);
+  }
+}
+function selected_part_changed() {
+  console.log("changed part");
+}
+
+function pressed_jump_scene() {
+  jump_to(
+    to_seconds(
+      scenes[ui_select_scene.value()].parts[ui_select_part.value()].start
+    )
+  );
+}
+
+function pressed_jump_time() {
+  jump_to(to_seconds(ui_input_time.value()));
+}
+
+function jump_to(time) {
+  player.setCurrentTime(time);
+}
+
+function to_seconds(string) {
+  let s2 = split(string, ":");
+  s2 = float(s2);
+  let time = 0;
+  time += s2[0] * 3600;
+  time += s2[1] * 60;
+  time += s2[2] * 1;
+  return time;
+}
+
+function pressed_drop_down() {
+  if (!toolbar.visible) {
+    show_toolbar();
+  } else if (toolbar.visible) {
+    hide_toolbar();
+  }
+}
+
+function show_toolbar() {
+  toolbar.posY = float(iframe.height);
+  ui_jump_scene.position(toolbar.posX, toolbar.posY);
+  ui_jump_scene.size(50 * ui_scale, 20 * ui_scale);
+  ui_jump_scene.style("font-size", 10 * ui_scale + "px");
+
+  ui_select_scene.position(toolbar.posX + 55 * ui_scale, toolbar.posY);
+  ui_select_scene.size(100 * ui_scale, 10 * ui_scale);
+  ui_select_scene.style("font-size", 8 * ui_scale + "px");
+
+  ui_select_part.position(
+    toolbar.posX + 55 * ui_scale,
+    toolbar.posY + 10 * ui_scale
+  );
+  ui_select_part.size(100 * ui_scale, 10 * ui_scale);
+  ui_select_part.style("font-size", 8 * ui_scale + "px");
+
+  ui_input_time.position(
+    toolbar.posX + 55 * ui_scale,
+    toolbar.posY + 25 * ui_scale
+  );
+  ui_input_time.size(97 * ui_scale, 10 * ui_scale);
+  ui_input_time.style("font-size", 8 * ui_scale + "px");
+
+  ui_jump_time.position(toolbar.posX, toolbar.posY + 25 * ui_scale);
+  ui_jump_time.size(50 * ui_scale, 20 * ui_scale);
+  ui_jump_time.style("font-size", 10 * ui_scale + "px");
+
+  
+  
+  
+  ui_text_custom_features.position(toolbar.posX, toolbar.posY + 40 * ui_scale);
+  ui_text_custom_features.size(80 * ui_scale, 12 * ui_scale);
+  ui_text_custom_features.style("font-size", 10 * ui_scale + "px");
+  
+  
+  
+  ui_save_timestamp.position(toolbar.posX, toolbar.posY + 70 * ui_scale);
+  ui_save_timestamp.size(50 * ui_scale, 13 * ui_scale);
+  ui_save_timestamp.style("font-size", 7* ui_scale + "px");
+  
+  
+  ui_save_timestamp_name.position(toolbar.posX + 55 * ui_scale, toolbar.posY + 70 * ui_scale);
+  ui_save_timestamp_name.size(100 * ui_scale, 10 * ui_scale);
+  ui_save_timestamp_name.style("font-size", 8* ui_scale + "px");
+  
+  
+  toolbar.visible = true;
+  //console.log(iframe. height+1)
+}
+
+function hide_toolbar() {
+  ui_jump_scene.position(-10000, -10000);
+  ui_jump_scene.size(0, 0);
+
+  ui_select_scene.position(-10000, -10000);
+  ui_select_part.position(-10000, -10000);
+  ui_input_time.position(-10000, -10000);
+
+  ui_jump_time.position(-10000, -10000);
+  ui_text_custom_features.position(-10000,-10000)
+  ui_save_timestamp.position(-10000, -10000);
+  ui_save_timestamp_name.position(-10000, -10000);
+  toolbar.visible = false;
+}
+
+function resize_video(w) {
+  video_width = w; // video width
+  video_height = video_width / video_ratio;
+
+  iframe.style.width = video_width + "px";
+  iframe.style.height = video_height + "px";
+}
+
+function scale_elements() {
+  video_width = windowWidth; // video width
+  video_height = video_width / video_ratio;
+
+  iframe.style.width = video_width + "px";
+  iframe.style.height = video_height + "px";
+
+  ui_select_scene.position(20, video_height + 10);
+
+  ui_jump_scene.position(sel.size().width + 20, video_height + 10);
+  ui_input_time.position(20, video_height + 40);
+  ui_jump_time.position(sel.size().width + 20, video_height + 40);
+  ui_input_time.size(sel.size().width - 7, 20);
+}
+
+function config_mobile_landscape() {
+  resize_video(3500);
+  toolbar.posX = 3510;
+}
+
+function windowResized() {
+  hide_toolbar();
+  if (deviceOrientation == "landscape") {
+    config_mobile_landscape();
+  }
+  if (deviceOrientation == "portrait") {
+    config_mobile_portrait();
+  }
+}
+
+function config_mobile_portrait() {
+  resize_video(1920);
+  toolbar.posX = 0;
+}
+
+function empty_select() {
+  ui_select_part.elt.innerHTML = "";
+}
+
+function to_string(seconds) {
+  let string = "";
+  string += floor(seconds / 3600);
+  seconds = seconds % 3600;
+  string += ":";
+
+  string += floor(seconds / 60);
+  seconds = seconds % 60;
+  string += ":";
+  string += floor(seconds / 1);
+  seconds = seconds % 1;
+  return string;
+}
+
+function get_current_timestamp() {}
+
+function create() {}
+
+function pressed_save_timestamp() {
+  player.getCurrentTime().then(seconds => {
+
+    let name = ui_save_timestamp_name.value();
+    let parts = scenes[scenes.length - 1].parts;
+
+    // Check for duplicate by name
+    if (parts.some(p => p.name === name)) {
+      console.log("duplicate");
+      return; // stop here if you want
+    }
+
+    console.log("saved timestamp " + to_string(floor(seconds)) + " with name '" + name + "'");
+
+    parts.push({
+      type: "event",
+      name: name,
+      index: 5,
+      start: to_string(floor(seconds)),
+      end: to_string(floor(seconds)),
+    });
+
+  });
+}
+
